95da1c6ab62707267b02212c2f3b97bfa8c2f0ef
Hernando Mueses <hernandomueses@esap.edu.co>
2021-10-09 15:02:40 -0500

    commit  code creando form

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,3 +1,56 @@
 <template>
-    <h1>Hello world 2 y todo el codigo que viene gracias Dios</h1>
+    <div>
+        <nav class="navbar navbar-ligth bg-litgh">
+        <a class="navbar-brand" href="/" >Task Desk</a>
+        </nav>
+
+        <div class="container">
+            <div class="row pt-5">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <form @submit.prevent="addTask">
+                                <div class="form-group">
+                                    <input type="text"
+                                    v-model="task.title" 
+                                    placeholder="Insert A Task"
+                                    class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <textarea v-model="task.description" 
+                                    cols="30" rows="10"
+                                    class="form-control"
+                                    placeholder="Insert A Description"></textarea>
+                                </div>
+                                <button class="btn btn-primary
+                                btn-block">Send</button>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                task: {
+                    title:'',
+                    description: ''
+                }
+            }
+        },
+        methods: {
+            addTask() {
+                console.log(this.task)
+            }
+        }
+    }
+
+</script>
